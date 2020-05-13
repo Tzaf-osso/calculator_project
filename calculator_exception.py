0ba1dfd9af8f6525850f8b121168821d0ff7be96
Tzaf-osso <tzafrir4321@gmail.com>
2020-05-13 14:21:51 +0300

    add exceptions

--- calculator_exception.py
+++ calculator_exception.py
@@ -1,9 +1,28 @@
-class Invalid_char(Exception):
-    """Raised when the input value is too small"""
+class InvalidChar(Exception):
+    """
+    Raised when there is char that not ok
+    good char:
+    - digit
+    - ()
+    - + - * / ^ ! % ~
+    """
     pass
 
 
-class Num_after_num(Exception):
-    """Raised when the input value is too large"""
+class NumAfterNum(Exception):
+    """
+    Raised when there is number after number with space
+    like:
+    8+2+3 4*5/67
+    """
     pass
 
+
+class ExpressionOrder(Exception):
+    """
+    Raised when there is problem with the expression like:
+    5+*8
+    6/*2
+    7(*9+2)
+    """
+    pass
